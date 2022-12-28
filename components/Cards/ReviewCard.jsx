34644db5cd6b87260aa6dbe8e31033f529c36c5e
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-12-28 14:53:13 +0100

    init commit

--- components/Cards/ReviewCard.jsx
+++ components/Cards/ReviewCard.jsx
@@ -2,60 +2,69 @@ import React from 'react';
 import CardDisplay from "./CardDisplay";
 import Stars from "../Stars";
 import Image from "next/image";
-import {reviewData} from "../../reviewData";
+
+
 import nicolle from '../../public/img/nicolle.jpeg'
+import margriet from '../../public/img/margriet.jpeg'
+import richard from '../../public/img/richard.jpeg'
+import annemarie from '../../public/img/annemarie.jpeg'
+import derkelina from '../../public/img/derkelina.jpeg'
+import anne from '../../public/img/anne.jpeg'
+import renate from '../../public/img/renate.jpeg'
+import manuela from '../../public/img/manuela.jpeg'
+import ellen from '../../public/img/ellen.jpeg'
 const ReviewCard = () => {
     const reviewData = [
         {
-            img: "/../public/img/nicolle.jpeg",
+            img: nicolle,
             name: 'Nicolle Stevens',
             rating: <Stars/>,
             review: 'Ze is altijd vriendelijk en vrolijk. Ze kijkt welk model het beste bij je past. Ben altijd super tevreden als ik bij haar weg ga. Top kapper.❤️'
         },
         {
-            img: "/../public/img/margriet.jpeg",
+            img: margriet,
             name: 'Margriet Meijerhof-Huizinga',
             rating: <Stars/>,
             review: 'Femke kijkt echt naar wat bij je past zowel kwa model als kleur. echt super.'
         },
         {
-            img: "/../public/img/richard.jpeg",
+            img: richard,
             name: 'Richard Martine Matthijs Raphaels',
             rating: <Stars/>,
             review: 'Leuke kapster. Knipt goed. Kan ook goed met krullend haar overweg. En ze doet het geweldig met de kinderen!!'
         },
         {
-            img: "/../public/img/annemarie.jpeg",
+            img: annemarie,
             name: 'Annemarie Agema',
             rating: <Stars/>,
             review: 'Super kapster, gezellig en mijn kinderen vinden haar geweldig!!'
         },
         {
-            img: "/../public/img/derkelina.jpeg",
+            img: derkelina,
             name: 'Derkelina Bul',
             rating: <Stars/>,
             review: 'Ik schrijf wel recensie. Mijn haar zit immers iedere keer perfect. Moeilijk haar. Eigen krul. Ook heb ik nu wenkbrauwen. Ben er zo blij mee. Djw. Tot de volgende keer.'
         },
         {
-            img: "/../public/img/anne.jpeg",
+            img: anne,
             name: 'Anne Wijers',
             rating: <Stars/>,
             review: 'Geweldige kapsalon, ben zeer tevreden en kom er al jaren.'
         },
         {
-            img: "/../public/img/renate.jpeg",
+            img: renate,
             name: 'Renate Rijploeg',
             rating: <Stars/>,
             review: 'Altijd gezellig en dik tevreden bij Femke!'
         },
         {
-            img: "/../public/img/manuela.jpeg",
+            img: manuela,
             name: 'Manuela van der Werf',
             rating: <Stars/>,
             review: 'Super gezellig en professioneel!'
         },
         {
-            img: "/../public/img/ellen.jpeg",
+            img: ellen,
             name: 'Ellen Nissink',
             rating: <Stars/>,
             review: 'Fijne kapper, altijd naar tevredenheid!'
