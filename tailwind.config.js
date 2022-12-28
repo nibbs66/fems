c58bc803b598443f07cf3e79b6ee049801b329c5
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-12-28 13:51:35 +0100

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'bgImg': "url('../public/img/femsachtergrond.jpeg')",
+            },
+            fontFamily:{
+                nunito:["Nunito", "sans-serif"],
+            },
+            backgroundSize: {
+                '400%': '400%'
+            },
+            animation: {
+              'colorTransition': 'bgScroll 10s infinite alternate',
+               tilt: 'tilt 10s infinite linear'
+            },
+            keyframes :{
+            bgScroll:{
+                '0%': {'backgroundPosition': 'left'},
+                '100%': {'backgroundPosition': 'right'},
+            },
+                tilt: {
+                "0%, 50%, 100%": {
+                    transform: 'rotate(0deg)',
+                },
+                    '25%': {
+                        transform: 'rotate(1deg)',
+
+
+
+                    },
+                    '75%': {
+                        transform: 'rotate(-1deg)',
+                    }
+                }
+            },
+
+            },
+
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+}
